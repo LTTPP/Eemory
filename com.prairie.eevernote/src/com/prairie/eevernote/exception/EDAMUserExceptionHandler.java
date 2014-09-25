314909d613d1b0c9bbb6371a5d1a12e1a448e7aa
Jianwei Liu <liujianwei.me@google.com>
2014-09-25 16:57:57 +0800

    refactor

--- com.prairie.eevernote/src/com/prairie/eevernote/exception/EDAMUserExceptionHandler.java
+++ com.prairie.eevernote/src/com/prairie/eevernote/exception/EDAMUserExceptionHandler.java
@@ -2,8 +2,6 @@ package com.prairie.eevernote.exception;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
 import com.evernote.edam.error.EDAMErrorCode;
@@ -11,6 +9,7 @@ import com.evernote.edam.error.EDAMUserException;
 import com.prairie.eevernote.Constants;
 import com.prairie.eevernote.EEPlugin;
 import com.prairie.eevernote.Messages;
+import com.prairie.eevernote.util.EclipseUtil;
 import com.prairie.eevernote.util.LogUtil;
 
 public class EDAMUserExceptionHandler {
@@ -19,24 +18,14 @@ public class EDAMUserExceptionHandler {
         if (e.getErrorCode() == EDAMErrorCode.AUTH_EXPIRED) {
             return new Status(Status.ERROR, EEPlugin.PLUGIN_ID, Messages.getString(Constants.PLUGIN_RUNTIME_AUTHEXPIRED_MESSAGE));
         }
-        return LogUtil.error(e);// TODO maybe e.toString()
+        return LogUtil.error(e);
     }
 
     public void handleDesingTime(final Shell shell, final EDAMUserException e) {
         if (e.getErrorCode() == EDAMErrorCode.AUTH_EXPIRED) {
-            Display.getDefault().syncExec(new Runnable() {
-                @Override
-                public void run() {
-                    MessageDialog.openError(shell, Messages.getString(Constants.PLUGIN_RUNTIME_AUTHEXPIRED_TITLE), Messages.getString(Constants.PLUGIN_RUNTIME_AUTHEXPIRED_MESSAGE));
-                }
-            });
+            EclipseUtil.openErrorSync(shell, Messages.getString(Constants.PLUGIN_RUNTIME_AUTHEXPIRED_TITLE), Messages.getString(Constants.PLUGIN_RUNTIME_AUTHEXPIRED_MESSAGE));
         } else {
-            Display.getDefault().syncExec(new Runnable() {
-                @Override
-                public void run() {
-                    MessageDialog.openError(shell, Messages.getString(Constants.PLUGIN_ERROR_OCCURRED), e.toString());
-                }
-            });
+            EclipseUtil.openErrorSync(shell, Messages.getString(Constants.PLUGIN_ERROR_OCCURRED), e.toString());
         }
     }
 
